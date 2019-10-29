6fc2d5c59eb29e7a3d20efd054ffddd185424a39
Ben Conway <benrconway84@gmail.com>
2019-10-29 16:17:12 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>TechStack App</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
